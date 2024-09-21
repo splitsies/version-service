e876272954bba369db716f0b0023239d33a6daa3
Kevin Chen <kevchen21@gmail.com>
2024-09-21 10:56:39 -0700

    publish message to connected topic

--- src/functions/get-min-supported-version/handler.ts
+++ src/functions/get-min-supported-version/handler.ts
@@ -1,8 +1,13 @@
 import "reflect-metadata";
-import { Logger, SplitsiesFunctionHandlerFactory } from '@splitsies/utils';
-import { DataResponse, HttpStatusCode } from "@splitsies/shared-models";
+import { IMessageQueueClient, Logger, SplitsiesFunctionHandlerFactory } from '@splitsies/utils';
+import { DataResponse, HttpStatusCode, QueueMessage } from "@splitsies/shared-models";
 import config from "../../config/version.config.json";
+import { container } from "../../di/inversify.config";
+import { randomUUID } from "crypto";
+
+const messageQueueClient = container.get<IMessageQueueClient>(IMessageQueueClient);
 
 export const main = SplitsiesFunctionHandlerFactory.create<never, string>(new Logger(), async (event) => {
+    await messageQueueClient.publish(new QueueMessage(process.env.UserConnectedTopicArn, randomUUID(), process.env.RtRegion));
     return new DataResponse(HttpStatusCode.OK, config.minSupportedVersion);
 });
