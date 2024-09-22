ba9c393665d019593612e9f3771299a7c917138a
Kevin Chen <kevchen21@gmail.com>
2024-09-21 19:02:53 -0700

    simple handler

--- src/functions/edge/notify-connection/handler.ts
+++ src/functions/edge/notify-connection/handler.ts
@@ -1,12 +1,5 @@
-import "reflect-metadata";
-import { Logger, MessageQueueClient, SplitsiesFunctionHandlerFactory } from '@splitsies/utils';
-import { DataResponse, HttpStatusCode, QueueMessage } from "@splitsies/shared-models";
-import { randomUUID } from "crypto";
-
-// const messageQueueClient = new MessageQueueClient(new Logger())
-
-export const main = SplitsiesFunctionHandlerFactory.create<never, string>(new Logger(), async (event) => {    
-    // await messageQueueClient.publish(new QueueMessage(process.env.UserConnectedTopicArn, randomUUID(), process.env.RtRegion));
-    console.log("The edge function has successfully been run");
-    return new DataResponse(HttpStatusCode.OK, "successfully published connection notification").toJson();
-});
+export const main = (event, context, callback) => {
+    console.log({ event, context });
+    const request = event.Records[0].cf.request;
+    callback(null, request);
+}
