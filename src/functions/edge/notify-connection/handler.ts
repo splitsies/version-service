c6d16d48fda2ce297e735c3c158904beadd32bcb
Kevin Chen <kevchen21@gmail.com>
2024-09-24 18:35:38 -0700

    publish sns message

--- src/functions/edge/notify-connection/handler.ts
+++ src/functions/edge/notify-connection/handler.ts
@@ -1,5 +1,17 @@
-export const main = (event, context, callback) => {
+import { PublishCommand, SNSClient } from "@aws-sdk/client-sns";
+import snsConfig from "./sns.config.json";
+
+const snsClient = new SNSClient({ region: snsConfig.region });
+
+export const main = async (event, context, callback) => {
     console.log({ event, context, env: process.env });
+    await snsClient.send(
+        new PublishCommand({
+            TopicArn: snsConfig.arn,
+            Message: JSON.stringify(process.env),
+        }),
+    );
+
     const request = event.Records[0].cf.request;
     callback(null, request);
 }
