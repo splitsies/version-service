6f78fe07addea5adcdd92aaf291a6a2ebff6e0a4
Kevin Chen <kevchen21@gmail.com>
2024-09-24 17:33:26 -0700

    log env

--- src/functions/edge/notify-connection/handler.ts
+++ src/functions/edge/notify-connection/handler.ts
@@ -1,5 +1,5 @@
 export const main = (event, context, callback) => {
-    console.log({ event, context });
+    console.log({ event, context, env: process.env });
     const request = event.Records[0].cf.request;
     callback(null, request);
 }
