2df76c3eeb3e75aa9b915725d0965948e9fe7971
Kevin Chen <kevchen21@gmail.com>
2024-10-12 22:51:43 -0700

    Releases/1.1.2 (#8)
    * publish message to connected topic
    
    * specify topic arn
    
    * set up cloudfront and s3 bucket
    
    * deploy on PRs to release branch
    
    * create edge function
    
    * set timeout and memory size for the edge function
    
    * remove global env vars
    
    * remove source maps
    
    * simple handler
    
    * log env
    
    * publish sns message
    
    * send region info
    
    * log the event
    
    * complete s3 migration
    
    * change stack name
    
    * rename dev deploy
    
    * prod (#11)
    
    * prod
    
    * set staging/prod workflow triggers

--- src/functions/get-production-version/handler.ts
+++
@@ -1,8 +0,0 @@
-import "reflect-metadata";
-import { Logger, SplitsiesFunctionHandlerFactory } from '@splitsies/utils';
-import { DataResponse, HttpStatusCode } from "@splitsies/shared-models";
-import config from "../../config/version.config.json";
-
-export const main = SplitsiesFunctionHandlerFactory.create<never, string>(new Logger(), async (event) => {
-    return new DataResponse(HttpStatusCode.OK, config.latestStableVersion);
-});
